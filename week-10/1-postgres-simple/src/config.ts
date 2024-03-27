10d4b4592019fd86f64bb9ce7b6dd3e74f79932c
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-03-27 13:59:44 +0530

    Update todos and user psql

--- week-10/1-postgres-simple/src/config.ts
+++ week-10/1-postgres-simple/src/config.ts
@@ -1,2 +1,2 @@
 
-export const DB_URL = "postgres://postgres:mysecretpassword@localhost/postgres";
+export const DB_URL = "postgresql://neondb_owner:J1asTjZGu4Fk@ep-calm-poetry-a123i742.ap-southeast-1.aws.neon.tech/neondb?sslmode=require";
